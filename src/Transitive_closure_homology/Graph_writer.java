ad452fb6bc98f7c8753a975d6419bd1a239fd062
Naheed <naheedcse@gmail.com>
2016-02-24 22:52:47 +0800

    Bug fixes in graph writer class

--- src/Transitive_closure_homology/Graph_writer.java
+++ src/Transitive_closure_homology/Graph_writer.java
@@ -17,6 +17,7 @@ import java.util.logging.Logger;
  * @author naheed
  */
 public class Graph_writer {
+    //write the graph provided by the adjacency matrix as an edgelist
     static void write_graph(boolean adj_mat[][],String filename,String option){
         File fobj;
         FileWriter fwriterobj = null;
@@ -26,8 +27,41 @@ public class Graph_writer {
             fwriterobj = new FileWriter(fobj);
              writer = new BufferedWriter(fwriterobj);
             if(option.equalsIgnoreCase("edgelist")){
-                System.out.println("hi"+adj_mat.length);
+                //System.out.println("hi"+adj_mat.length);
                 //write the graph as an edgelist.
+                for(int i = 0 ; i<adj_mat.length; i++){
+                    for (int j = i; j < adj_mat[i].length; j++) {
+                        if(adj_mat[i][j]){
+                            writer.write(String.valueOf(i)+" "+String.valueOf(j)+"\n");
+                        }
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Graph_writer.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                //fwriterobj.close();
+                writer.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Graph_writer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    static void write_graph(boolean adj_mat[][],String filename,String option,int []nodelist){
+        File fobj;
+        FileWriter fwriterobj = null;
+        BufferedWriter writer = null ;
+        try {
+            fobj = new File(filename);
+            fwriterobj = new FileWriter(fobj);
+             writer = new BufferedWriter(fwriterobj);
+            if(option.equalsIgnoreCase("edgelist")){
+                //System.out.println("hi"+adj_mat.length);
+                //write the graph as an edgelist.
+                for (int o : nodelist) {
+                    writer.write(o+"\n");
+                }
                 for(int i = 0 ; i<adj_mat.length; i++){
                     for (int j = i; j < adj_mat[i].length; j++) {
                         if(adj_mat[i][j]){
